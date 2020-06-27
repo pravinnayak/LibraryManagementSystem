4f91be2fb1d3ceffea574da635bd7611ba7c7d6a
pravinnayak <pra12vin54@gmail.com>
2020-06-27 19:03:23 +0530

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -4,11 +4,11 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
-    uri:"mongodb+srv://cluster0-sgxqj.gcp.mongodb.net/mean-dev",
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
+    //uri:"mongodb+srv://cluster0-sgxqj.gcp.mongodb.net/mean-dev",
     options: {
-      username:"dbUser",
-      password:"dbUserPassword"
+      //username:"dbUser",
+      //password:"dbUserPassword"
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
